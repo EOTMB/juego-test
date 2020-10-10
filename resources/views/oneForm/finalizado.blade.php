0141b3d91627ff785b9d61017078beb284fd8681
EOTMB <agustinmorelli88@gmail.com>
2020-10-09 23:50:54 -0300

    remove old files

--- resources/views/oneForm/finalizado.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<h1>Tu puntaje fue {{$score}}</h1>
-<a href="{{route('iniciar_unico')}}">Volver al inicio</a>
-@endsection
--- resources/views/oneQuestion/finalizado.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<h1>Tu puntaje fue {{Session::get('score')}}</h1>
-<a href="{{route('iniciar')}}">Volver al inicio</a>
-@endsection
